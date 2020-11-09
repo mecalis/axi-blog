d8838d4a6a0e2c81c5a0b3ec2aed8c48c5e44623
Tamas <mecalis@gmail.com>
2020-11-09 14:06:55 +0100

    a

--- tasks/models.py
+++ tasks/models.py
@@ -47,7 +47,7 @@ class TaskListModel3(models.Model):
     list_title = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.list_title
+        return str(self.list_title)
 
 class Task3(models.Model):
     tasklist = models.ForeignKey(TaskListModel3, on_delete=models.CASCADE, null=True, blank=True)
@@ -56,4 +56,4 @@ class Task3(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.task_title
+        return str(self.task_title)
